5fb692522ce8c691e8fd6e75c8d32ca32e5517b5
Artyom Shaternik <artemsh1109@gmail.com>
2026-07-06 15:42:41 +0300

    delete comments

--- practice1/mainwindow.h
+++ practice1/mainwindow.h
@@ -22,35 +22,34 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow() {}
 
-    // Метод сортировки пузырьком по убыванию (Вариант 14)
     void bubbleSortDescending(int n);
     QVector<Student> getStudents() const { return students; }
     void setStudents(const QVector<Student>& newStudents) { students = newStudents; }
 
 protected:
-    void keyPressEvent(QKeyEvent *event) override; // П.3: Обработка нажатий клавиш
+    void keyPressEvent(QKeyEvent *event) override;
 
 private slots:
     void addStudent();
     void removeSelected();
     void sortStandard();
     void sortBubbleVariant();
-    void showContextMenu(const QPoint &pos); // П.5: Обработка правой кнопки мыши
+    void showContextMenu(const QPoint &pos);
 
 private:
     void updateWidgets();
 
     QVector<Student> students;
 
-    QLineEdit *sizeInput;     // П.1: Размер массива
+    QLineEdit *sizeInput;
     QLineEdit *surnameInput;
     QSpinBox *courseInput;
     QSpinBox *phoneInput;
 
-    QTableWidget *tableWidget; // П.7, П.8
-    QListWidget *listWidget;   // П.7, П.9
-    QComboBox *comboBox;       // П.6: Четный вариант
-    QLabel *imageLabel;        // П.4: Отображение картинки
+    QTableWidget *tableWidget;
+    QListWidget *listWidget;
+    QComboBox *comboBox;
+    QLabel *imageLabel;
 };
 
 #endif // MAINWINDOW_H
